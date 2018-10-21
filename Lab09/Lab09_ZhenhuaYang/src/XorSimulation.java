861cce4eb53daf38bb9a9abe31926ec13f7f96c7
Aaron Yang <hua3467@yahoo.com>
2018-10-20 22:02:24 -0500

    XOR simulation my solution

--- Lab09/Lab09_ZhenhuaYang/src/XorSimulation.java
+++ Lab09/Lab09_ZhenhuaYang/src/XorSimulation.java
@@ -8,7 +8,7 @@ public class XorSimulation
 {
     public static void main( String[] args )
     {
-        char input1;
+        char result;
         char input2;
         String input;
         
@@ -18,24 +18,23 @@ public class XorSimulation
         input = scan.next();  
        
         // assume 101
-         input1 = input.charAt(0);
+         result = input.charAt(0);
          
         for( int i = 1; i < input.length(); i++ )
         {
             input2 = input.charAt(i);
 
-            if ( input1 != input2  )
+            if ( result == input2  )
             {
-                input1 = '1';
+                result = '0';
             }
-            
-            if ( input1 == input2 )
+            else
             {
-                input1 = '0';
+                result = '1';
             }
         }
         
-        System.out.println( "The result is " + input1 ); 
+        System.out.println( "The result is " + result ); 
         
     }
    
