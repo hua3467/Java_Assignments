d7df718cd18d8942899ace1f285538b3224c31f4
Aaron Yang <hua3467@yahoo.com>
2018-11-20 15:46:42 -0600

    Lab 3 finished

--- Lab13/Lab13/Lab13_2018/src/SalesTax.java
+++ Lab13/Lab13/Lab13_2018/src/SalesTax.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author ZhenhuaYang
+ * @group of ZhenhuaYang, YingyaLi, AmrithaRaveendran
  */
 import java.text.DecimalFormat;
 
@@ -8,13 +8,45 @@ public class SalesTax {
     
     public static void main( String [] args ){
 
+        DecimalFormat twoDecimals = new DecimalFormat("0.00");
+        
+        // i. Create a 2D double array variable named US-States 
+        // and assign it the value returned by create2DArray() static method.
         double [][] usStates = create2DArray();
         
-        System.out.println( getString(usStates));
+        // ii. Print the string returned by getString() static method 
+        System.out.println( "\nPrint the string: \n" + getString(usStates));
+        
+        // iii. Print the string returned by taxRatesForSpecificState() 
         System.out.println( "\ntaxRatesForSpecificState: " + taxRatesForSpecificState( usStates, 6));
+        
+        // iv. Print the string returned by taxRatesForSpecificYear() 
         System.out.println( "\ntaxRatesForSpecificYear: \n" + taxRatesForSpecificYear( usStates, 7));
+        System.out.println();
+        
+        // v.Print the value returned by stateWithHighestTaxRate() 
+        System.out.println( "\nState with Highest Tax Rate: \n" + stateWithHighestTaxRate(usStates));
+        System.out.println();
+        
+        // vi.	Create an int array variable statesWithLowSalesTaxRate 
+        // and assign it the value returned by statesWithLowSalexTaxRate()
+        int [] statesWithLowSalesTaxRate = statesWithLowSalexTaxRate(usStates);
+        // Print the array
+        System.out.println("\nStates with Low Sales Tax Rate: ");
+        for( int i = 0; i < statesWithLowSalesTaxRate.length; i++ ){
+            System.out.println(statesWithLowSalesTaxRate[i]);
+        }
+        
+        // vii.	Create a double array variable statesHighestSalesTaxRate 
+        // and assign it the value returned by statesHighestSalesTaxRate ().
+        double [] statesHighestSalesTaxRate  = statesHighestSalesTaxRate(usStates);
+        // Print the array
+        System.out.println("\nHighst Sales Tax Rate of each state over the 10 years: ");
+        for( int i = 0; i < statesHighestSalesTaxRate .length; i++ ){
+            System.out.println(twoDecimals.format(statesHighestSalesTaxRate [i]));
+        }
+  
     }
-
 
 
     public static double[][] create2DArray(){
@@ -39,10 +71,9 @@ public class SalesTax {
         String str = "";
         for( int i = 0; i < array.length; i++){
             for( int j = 0; j < array[i].length; j++ ){
-                if( j % 10 == 0 )
-                    str += "\n";
                 str += twoDecimal.format(array[i][j]) + "\t" ;  // format the elements in the 2D-array
             }
+            str += "\n";
         }
         
         return str;
@@ -68,11 +99,12 @@ public class SalesTax {
         DecimalFormat twoDecimal = new DecimalFormat("0.00");
 
         for( int i = 0; i < array.length; i++ ){
-            if( i % 10 == 0 )
-                str += "\n";
+            if( i % 10 == 0 && i !=0 )
+                str += twoDecimal.format(array[i][yearIndex]) + "\n";
             else
                 str += twoDecimal.format(array[i][yearIndex]) + "\t";
         }
+        
         return str;
     }
     
@@ -80,27 +112,57 @@ public class SalesTax {
         
         double maxAvg = 0;
         double sumYear = 0;
-        double avgRate = 0;
         int maxIndex = 0;
         
         for( int i = 0; i < array.length; i++ ){
+            
             for( int j = 0; j < array[i].length; j++ ){
                 sumYear += array[i][j];   
             }
-            avgRate = sumYear / array[i].length;
-                if( maxAvg < avgRate )
+            
+                if( maxAvg < sumYear / array[i].length ){
                     maxIndex = i;
+                    maxAvg = sumYear / array[i].length;
+                }
         }
         return maxIndex;
     }
     
     public static int[] statesWithLowSalexTaxRate( double [][] array ){
-        int[] arr = new int [10];
-        return arr;
+        int[] arr = new int [array.length];
+        int count = 0;
+        
+        for( int i = 0; i < array.length; i++ ){
+            for( int j = 0; j < array[i].length; j++ ){
+                if( array[i][j] < 0.001 ){
+                    arr[count] = i;
+                    count++;
+                    break;
+                }
+            }
+        }
+        
+        int [] temp = new int[count];
+        for( int i = 0; i < count; i++ ){
+            temp[i] = arr[i];
+        }
+        
+        return temp;
     }
     
-    public static double[] statesHighestSalesTaxRate(){
-        double[] arr = new double[10];
+    public static double[] statesHighestSalesTaxRate( double [][] array ){
+        double[] arr = new double[array.length];
+        double max = 0;
+        
+        for(int i = 0; i < array.length; i++){
+            for( int j = 0; j < array[i].length; j++ ){
+                if( max < array[i][j]){
+                    max = array[i][j];
+                }
+                arr[i]= max;
+                max = 0;
+            }
+        }
         return arr;
     }
 }
